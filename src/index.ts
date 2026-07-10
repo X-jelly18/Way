fffdba290dfbef04b57b8a18b7cc0ecfbdc886a8
X-jelly18 <elitegamer128k@gmail.com>
2026-07-10 10:03:19 +0200

    Replace '/btc' route with '/p6CAaNg' route

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const ROUTES: RouteMap = {
   "/maibhhhh": "http://south.ayanakojivps.shop",
   "/s1": "http://south2.ayanakojivps.shop",
   "/s2": "http://south3.ayanakojivps.shop",
-  "/btc": "https://102.202.192.103",
+  "/p6CAaNg": "https://six.ayanakojivps.shop",
 };
 
 function getTarget(pathname: string): string | null {
